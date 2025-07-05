8582ef6d11b44629bb1706976b253582e08c8eb9
Juan Miguel Acosta Ortega <acostaojuanmi@gmail.com>
2025-07-05 17:44:47 +0200

    fix : adding lot in sale

--- app/models/sale.py
+++ app/models/sale.py
@@ -7,6 +7,7 @@ class Sale(db.Model):
     sale_date = Column(Date , primary_key=True)
     article_name = Column(String(50), nullable=False, unique=False)
     article_unit = Column(String(50), nullable=False, unique=False)
+    article_lot = Column(String(50), nullable=True, unique=False)  # Optional field for article lot
     client_name = Column(String(50), nullable=False, unique=False)
     price_unit = Column(Double, nullable=False, unique=False)
     quantity = Column(Double, nullable=False, unique=False)
@@ -17,7 +18,7 @@ class Sale(db.Model):
     # ORM relationships
     user = db.relationship('User', back_populates='sales')
 
-    # Unique constraint to ensure two user can have the smae article name
+    # Unique constraint to ensure two user can have the same article name
     __table_args__ = (
         UniqueConstraint('user_id', 'sale_date', 'article_name', 'client_name', name='_user_sale_name_uc'), # means in a sale from a specific date can't repeart the same article name and client name
     )
